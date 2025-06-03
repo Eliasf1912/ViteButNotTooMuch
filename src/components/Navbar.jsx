e7f3895211a8060770a47449e7940052d8c18d4e
EliasF1912 <e.duvernois2016@gmail.com>
2025-06-03 15:28:26 +0200

    Ajout de tailwindcss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <nav>
-      <div><strong>VBN™</strong></div>
-      <div>
-        <NavLink to="/" end>Accueil</NavLink>
-        <NavLink to="/studio">Le studio</NavLink>
-        <NavLink to="/prestations">Prestations</NavLink>
-        <NavLink to="/projets">Projets</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-      </div>
-    </nav>
-  );
+	return (
+		<nav>
+			<div>
+				<strong>VBN™</strong>
+			</div>
+			<div>
+				<NavLink to='/' end>
+					Accueil
+				</NavLink>
+				<NavLink to='/studio'>Le studio</NavLink>
+				<NavLink to='/prestations'>Prestations</NavLink>
+				<NavLink to='/projets'>Projets</NavLink>
+				<NavLink to='/contact'>Contact</NavLink>
+			</div>
+		</nav>
+	);
 }
